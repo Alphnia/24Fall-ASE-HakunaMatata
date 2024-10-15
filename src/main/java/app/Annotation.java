b6c39af52201a048d54ee695cbee027650023112
yinyi <xiantanmengluo@outlook.com>
2024-10-15 10:52:06 -0400

    initial

---
+++ src/main/java/app/Annotation.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class is annotation for routes.
+ */
+@RestController
+public class Annotation {
+
+  public Annotation(int annoID, int routeID, int userID, List<Map<String, Object> > StopList){
+    this.annoID = annoID;
+    this.routeID = routeID;
+    this.userID = userID;
+    this.StopList = StopList;
+  }
+  public int getAnnoID(){
+    return this.annoID;
+  }
+
+  public int getRouteID(){
+    return this.routeID;
+  }
+  
+  public int getUserID(){
+    return this.userID;
+  }
+  public List<Map<String, Object>> getStopList (){
+    return this.StopList;
+  }
+  
+    private int annoID;
+    private int routeID;
+    private int userID;
+    private List<Map<String, Object>> StopList; 
+}
---
+++ src/test/java/app/RouteControllerUnitTests.java
@@ -0,0 +1,21 @@
+package app;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+/** 
+ * This is the unit test class.
+*/
+@SpringBootTest
+@ContextConfiguration
+public class RouteControllerUnitTests {
+
+  @BeforeAll
+  public static void setupCourseForTesting() {
+    
+  }
+
+}
+
