7121644ad00cce26026017b5848baaeffb7d5010
ashleychen628 <ashley.cdy628@gmail.com>
2024-11-25 16:26:02 -0500

    finished start and stop location sharing part

--- frontend/journey-ease/src/components/ShareLocation/index.jsx
+++ frontend/journey-ease/src/components/ShareLocation/index.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import { startShareLocation, stopShareLocation } from "./watchPosition";
 
 const ShareLocaion = () => {
-// function RouteDisplay(props) {
+  const [tracking, setTracking] = useState(false);
+
+  const startTracking = () => {
+    startShareLocation();
+    setTracking(true);
+  }
+
+  const stopTracking = () => {
+    stopShareLocation();
+    setTracking(false);
+  }
+
   return (
     <div>
-      <Button variant="contained" endIcon={<SendIcon />}>
-        Send
+      <Button variant="contained" endIcon={<SendIcon />} onClick={startTracking}>
+        Share Location
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />} onClick={stopTracking}>
+        Stop Share Location
       </Button>
     </div>
   );
